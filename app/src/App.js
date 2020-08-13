c4d0d345685f1a83f84ef3034fd4a56b3990b7f0
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-08-12 21:24:29 -0400

    working on axios

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Quotes />
-      <img src = 'https://pagesix.com/wp-content/uploads/sites/3/2018/10/181030-kanye-west-politics-feature.jpg?quality=90&strip=all&w=1200' />
+      {/* <img src = 'https://pagesix.com/wp-content/uploads/sites/3/2018/10/181030-kanye-west-politics-feature.jpg?quality=90&strip=all&w=1200' /> */}
     </div>
   );
 }
--- app/src/components/Quotes.js
+++ app/src/components/Quotes.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux'
 
 const Quotes = (props) => {
+    useEffect(() => {
+        // call action creator that will be in charge of fetching data
+    }, [])
     return (
         <section>
             <h1>🐻Daily Kanye Tweets🐻</h1>
+            {props.isLoading ? <h4>Loading Kanye's Tweets now...</h4> : null}
+            {props.error ? <p style={{ color: 'red' }}>GEORGE BUSH DOESN"T CARE ABOUT BLACK PEOPLE</p> : null}
+            {props.quotes.length > 0 ? <div /> : null}
         </section>
     );
 };
 
-export default Quotes;
+const mapStateToProps = (state) => {
+    return {
+        quotes: state.quotes,
+        isLoading: state.isLoading,
+        error: state.error
+    }
+}
+
+export default connect(mapStateToProps, {})(Quotes);
