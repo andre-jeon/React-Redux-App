d9271253a93a002529f0de722a0a2e1b3d9aea97
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-08-13 15:14:43 -0400

    last tweaks

--- app/src/components/Quotes.js
+++ app/src/components/Quotes.js
@@ -12,8 +12,9 @@ const Quotes = (props) => {
         <section>
             <h1>🐻Daily Kanye Tweets🐻</h1>
             {props.isLoading ? (<h4>Loading Kanye's Tweets now...</h4>) : null}
-            {props.error ? (<p style={{ color: 'red' }}>GEORGE BUSH DOESN"T CARE ABOUT BLACK PEOPLE</p>) : null}
-            {props.quotes.length > 0 ? <h2>{props.quotes}</h2> : null}
+            {props.error ? (<p style={{ color: 'red' }}>I CAN'T BE MANAGED</p>) : null}
+            {props.quotes.length > 0 ? <h2>"{props.quotes}"</h2> : null}
+            <button>GIVE ME ANOTHA ONE</button>
         </section>
     );
 };
